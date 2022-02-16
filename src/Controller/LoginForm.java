f761320b1a47ca9fec48e3347821d983a473df3b
jgautney <jmgautney23@gmail.com>
2022-02-16 15:20:19 -0800

    Added time zone id label to login form

--- src/Controller/LoginForm.java
+++ src/Controller/LoginForm.java
@@ -3,22 +3,33 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+
+
+import java.time.ZoneId;
 import java.util.ResourceBundle;
+import java.util.TimeZone;
 
 public class LoginForm implements Initializable{
     public TextField usernameTF;
     public TextField passwordTF;
     public Button loginButton;
     public Button exitButton;
+    public Label timeLabel;
+
+
+    //ZoneId zoneid = TimeZone.getDefault().toZoneId();
+    String zoneId = TimeZone.getDefault().getID();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    timeLabel.setText(String.valueOf(zoneId));
 
     }
+
 
     public void onLogin(ActionEvent actionEvent) {
         System.out.println("login button pressed!");
