3fc47bb54ceb8e4319e35c99d779bf1634ad8c7a
jgautney <jmgautney23@gmail.com>
2022-02-23 10:17:44 -0800

    implemented login check system and French language for error message on unsuccessful login

--- src/Controller/loginForm.java
+++ src/Controller/loginForm.java
@@ -1,10 +1,14 @@
 package Controller;
 
+import JDBC.JDBC;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,8 +17,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 
+import java.sql.*;
 
 import java.util.Locale;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import java.util.TimeZone;
 
@@ -46,17 +52,45 @@ public class loginForm implements Initializable{
     }
 
 
-    public void onLogin(ActionEvent actionEvent) throws IOException {
-        System.out.println("login button pressed!");
+    public void onLogin(ActionEvent actionEvent) {
+        String inputName = usernameTF.getText();
+        String inputPassword = passwordTF.getText();
 
-        Parent root = FXMLLoader.load(getClass().getResource("/View/customerForm.fxml"));
-        Stage stage =(Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-        stage.setTitle("Customers");
-        stage.setScene(new Scene(root, 1000, 600));
-        stage.show();
+        try{
+            String sql = "SELECT User_Name, Password from users";
+
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+
+            while (rs.next()) {
+                String userName = rs.getString("User_Name");
+                String password = rs.getString("Password");
+
+                if (Objects.equals(inputName, userName) && Objects.equals(inputPassword, password)) {
+
+                    Parent root = FXMLLoader.load(getClass().getResource("/View/customerForm.fxml"));
+                    Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+                    stage.setTitle("Customers");
+                    stage.setScene(new Scene(root, 1000, 600));
+                    stage.show();
+                    return;
+                    }
+                }
+
+                // If no successful login, show alert box
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Dialog");
+                alert.setContentText(rb.getString("match"));
+                alert.show();
+        }
+        catch (SQLException | IOException e){
+            e.printStackTrace();
+        }
     }
 
     public void onExit(ActionEvent actionEvent) {
+        JDBC.closeConnection();
         System.exit(0);
     }
 
