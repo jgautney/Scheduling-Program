7fb5a5c793a37dd9cc7ee67d530b462e1e700b8f
jgautney <jmgautney23@gmail.com>
2022-03-07 15:58:46 -0800

    implemented login recording to txt file

--- src/Controller/loginForm.java
+++ src/Controller/loginForm.java
@@ -17,13 +17,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 
 import java.sql.*;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 import java.util.Objects;
 import java.util.ResourceBundle;
@@ -45,6 +48,8 @@ public class loginForm implements Initializable{
     Locale locale = Locale.getDefault();
     ResourceBundle rb = ResourceBundle.getBundle("Labels", locale);
 
+    public LocalDateTime loginTime = LocalDateTime.now();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     timeLabel.setText(String.valueOf(zoneId));
@@ -57,15 +62,20 @@ public class loginForm implements Initializable{
     }
 
 
-    public void onLogin(ActionEvent actionEvent) {
+    public void onLogin(ActionEvent actionEvent) throws IOException {
         String inputName = usernameTF.getText();
         String inputPassword = passwordTF.getText();
+
+        FileWriter fwVariable = new FileWriter("login_activity.txt", true);
+        PrintWriter loginRecord = new PrintWriter(fwVariable);
 
         try{
             String sql = "SELECT User_Name, Password, User_ID from users";
 
             PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
+
+            boolean apptSoon = false;
 
             while (rs.next()) {
                 String userName = rs.getString("User_Name");
@@ -74,29 +84,36 @@ public class loginForm implements Initializable{
 
                 ObservableList<Appointments> appTime = DBAppointments.getAllAppointments(userID);
 
-                LocalDateTime loginTime = LocalDateTime.now();
-                ZoneId dtz = ZoneId.of("America/New_York");
-                ZoneId ctz = ZoneId.of("GMT");
-                LocalDateTime clt = loginTime.atZone(ctz).withZoneSameInstant(dtz).toLocalDateTime();
+
 
                 if (Objects.equals(inputName, userName) && Objects.equals(inputPassword, password)) {
-                    System.out.println(clt);
-                    System.out.println(LocalDateTime.now());
                     for (Appointments appointments : appTime) {
-                        if (appointments.getStart().isBefore(clt.plusMinutes(15))) {
 
+                        // Check for appointment within 15 minutes of login
+                        if (loginTime.isAfter(appointments.getStart().minusMinutes(15)) && loginTime.isBefore(appointments.getStart())) {
+                            apptSoon = true;
                             Alert alert = new Alert(Alert.AlertType.INFORMATION);
                             alert.setTitle("Information");
-                            alert.setContentText("You have an appointment soon " + appointments.getStart());
+                            alert.setContentText("You have an appointment soon. Appointment ID: " + appointments.getId() + " Time: " + appointments.getStart().toLocalTime() + " Date: " + appointments.getStart().toLocalDate());
                             alert.show();
-                        }
+                            }
                     }
 
+                    //if no appointment, display alert box
+                    if(!apptSoon){
+                        Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+                        alert1.setTitle("Information");
+                        alert1.setContentText("No upcoming appointments");
+                        alert1.show();
+                    }
                     Parent root = FXMLLoader.load(getClass().getResource("/View/customerForm.fxml"));
                     Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
                     stage.setTitle("Customers");
                     stage.setScene(new Scene(root, 1000, 600));
                     stage.show();
+
+                    loginRecord.println("Login Successful. User: " + userName + ", Time: " + loginTime.toLocalTime().truncatedTo(ChronoUnit.MINUTES) + ", Date: " + loginTime.toLocalDate());
+                    loginRecord.close();
                     return;
                         }
                 }
@@ -106,6 +123,8 @@ public class loginForm implements Initializable{
                 alert.setTitle("Error Dialog");
                 alert.setContentText(rb.getString("match"));
                 alert.show();
+            loginRecord.println("Login unsuccessful. User: " + inputName + ", Time: " + loginTime.toLocalTime().truncatedTo(ChronoUnit.MINUTES) + ", Date: " + loginTime.toLocalDate());
+            loginRecord.close();
 
         }
         catch (SQLException | IOException e){
